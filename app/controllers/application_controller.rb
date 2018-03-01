8892c6f89f78569854a2ef9abb4b6e367a52644b
Gabriel Lu <gabrielulu@gmail.com>
2018-03-01 11:23:11 +0800

    adjusted login redirect to storablelocation, and welcome index for login conditionals

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,26 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  protected
+  before_action :store_user_location!, if: :storable_location?
+  # before_action :authenticate_user!
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  protected
+    def after_sign_in_path_for(user)
+      stored_location_for(user) || super
+    end
+    def after_sign_out_path_for(user)
+      stored_location_for(user) || super
+    end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    end
+  private
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 end
