2b391b47fadadab6db163f873b98c23fbe8c5cbf
Gabriel Lu <gabrielulu@gmail.com>
2018-03-07 14:47:42 +0800

    adjust date display to GMT +8

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module YouComing
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    # change timezone default
+    config.time_zone = "Singapore"
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
