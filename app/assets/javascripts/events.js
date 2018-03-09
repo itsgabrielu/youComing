1aa2dabcd3901e9773cb2090da4fae19ebd298a1
Gabriel Lu <gabrielulu@gmail.com>
2018-03-09 16:30:05 +0800

    changed events.js to es5 to push to heroku

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,8 +1,8 @@
-$(document).on('turbolinks:load', () => {
+$(document).on('turbolinks:load', function () {
   const copyButton = document.getElementById('copy_url_btn')
   if (copyButton) {
     const copyText = document.getElementById('url_link')
-    copyButton.onclick = () => {
+    copyButton.onclick = function () {
       copyText.select()
       document.execCommand('Copy')
     }
